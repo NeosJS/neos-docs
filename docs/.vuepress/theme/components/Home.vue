276328485092afea3ca41fa5a57ac4118979f9d0
听着情歌流泪 <develop@lingtal.com>
2021-03-13 14:14:30 +0800

    🍑 🌴 🍋 —— 2021-03-13 14:14:30

--- docs/.vuepress/theme/components/Home.vue
+++ docs/.vuepress/theme/components/Home.vue
@@ -5,7 +5,7 @@
       <img v-if="data.heroImage" :src="$withBase(data.heroImage)" :alt="data.heroAlt || 'hero'" />
       <h1 v-if="data.heroText !== null" id="mainTitle" style="text-align:left;margin-left:36rem;width:50rem;height:60px;"></h1>
       <p class="description" id="description"  style="text-align:left;margin-left:36rem;width:58rem;height:33px;"></p>
-      <p class="description" id="line"  style="text-align:left;margin-left:36rem;width:60rem;height:33px;margin-top:-20px"></p>
+      <p class="description" id="line"  style="text-align:left;margin-left:36rem;width:66rem;height:33px;margin-top:-20px"></p>
       <!-- <p class="action" v-if="data.actionText && data.actionLink" style="text-align:left;margin-left:35rem;width:50rem"></p> -->
       <div class="features" v-if="data.features && data.features.length" style="margin-left:36rem;text-align:left">
       <div class="feature" v-for="(feature, index) in data.features" :key="index">
